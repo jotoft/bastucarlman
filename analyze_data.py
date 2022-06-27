9a48f45c18bfe64f6fe64409d3517c86e4d3c5a2
Johan Toft <johan.toft@gmail.com>
2022-06-27 09:13:05 +0200

    Add python scripts for analyzing and running kalman

---
+++ analyze_data.py
@@ -0,0 +1,92 @@
+#%%
+from datetime import datetime
+import pandas as pd
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+import matplotlib.pyplot as plt
+
+# Add argparser for token org and bucket
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--token", help="influxdb token", default="")
+parser.add_argument("--org", help="influxdb org", default="")
+parser.add_argument("--bucket", help="influxdb bucket", default="")
+args = parser.parse_args()
+
+# You can generate a Token from the "Tokens Tab" in the UI
+token = args.token
+org = args.org
+bucket = args.bucket
+
+client = InfluxDBClient(url="http://rollspelswiki.se:8086", token=token)
+
+
+query = rf"""from(bucket: "{bucket}")
+        |> range(start:-8h, stop: -4h)
+        |> filter(fn: (r) =>
+             r._measurement == "tempsensor" and
+             r._field == "temp" and
+            r.device == "cubecell"
+        )"""
+df = client.query_api().query_data_frame(query, org=org)
+#%%
+df['_value'].plot()
+plt.show()
+
+#%%
+time = df['_time']
+
+# Get delta times in seconds
+
+#%%
+test = time[1] - time[0]
+
+delta_times = [(t2 - t1).delta/1E9 for t1, t2 in zip(time, time[1:])]
+
+print(delta_times)
+
+
+#%%
+from model import Model
+
+md = Model(11.5)
+
+# Create new dataframe to collect kalman filtered values
+df_kalman = pd.DataFrame(columns=['temp', 'rate'])
+
+for dt, value in zip(delta_times, df['_value'][1:]):
+    m_, p_ = md.predict(dt)
+
+    #print(m_[0], m_[1])
+    m, p = md.update(value)
+    print(m[0], m[1]*60.0)
+
+    df_kalman.loc[len(df_kalman)] = [m[0], m[1]*60.0]
+
+# Use index from df_kalman to match with df
+df_kalman.index = df.index[1:]
+
+# Use the timestamps from df to index, they are in df['_time']
+df_kalman.index = df['_time'][1:]
+
+#%%
+# Plot the kalman filtered values
+# Plot rate in separate axis
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+
+# Plot the kalman filtered values
+ax1.plot(df_kalman['temp'], color='red')
+ax2.plot(df_kalman['rate'], color='blue')
+
+# Fix the messy axis labels
+ax1.set_ylabel('Temperature [°C]', color='red')
+ax2.set_ylabel('Rate [°C/min]', color='blue')
+
+ax2.set_xlabel('Time')
+
+
+# Show the plot
+plt.show()
+
+
---
+++ mqtt_kalman.py
@@ -0,0 +1,62 @@
+import json
+import time
+from model import Model
+
+# Connect using mqtt
+import paho.mqtt.client as mqtt
+
+# User argparser to get username and password for mqtt
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--username", help="mqtt username", default="")
+parser.add_argument("--password", help="mqtt password", default="")
+# Add hostname to the mqtt client
+parser.add_argument("--hostname", help="mqtt hostname", default="localhost")
+args = parser.parse_args()
+
+
+# Connect to broker at rollspelswiki.se
+broker_address = args.hostname
+client = mqtt.Client("kalman")
+
+client.username_pw_set(args.username, args.password)
+client.connect(broker_address)
+
+# Listen to topic "sauna/temp1"
+client.subscribe("sauna/temp1")
+
+# Create an initial kalman model
+md = Model(11.5)
+
+# Keep track of the time
+last_time = time.time()
+
+
+# Define a callback function for sauna temperature, which is a json message where temp2 contains the temperature
+def on_message(client, userdata, message):
+    global last_time
+    # Convert the message to a json object
+    message = json.loads(message.payload.decode("utf-8"))
+    # Get the temperature
+    temp = message["temp2"]
+    # Predict the temperature
+    seconds_elapsed = time.time() - last_time
+    last_time = time.time()
+    # Print seconds_elapsed
+    print(seconds_elapsed)
+
+    m, p = md.predict(seconds_elapsed)
+    # Update the kalman model
+    m, p = md.update(temp)
+    # Print the predicted and updated temperature
+    print("Temp:", m[0], "Rate:", m[1] * 60)
+
+    # Post the updated prediction to "sauna/temp1_prediction"
+    client.publish("sauna/temp1_prediction", json.dumps({"temp": m[0], "rate": m[1] * 60}))
+
+
+# Register the callback function
+client.on_message = on_message
+
+# Keep the program running
+client.loop_forever()
